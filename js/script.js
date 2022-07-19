41bd9d54f65792ecf5c4e4ab650059d3ae8951f8
Rohit Kumar <107547744+thisisrohitkumar@users.noreply.github.com>
2022-07-19 13:07:12 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function darkModeOn(){
+	document.getElementById('body').style.backgroundColor = "#333333";
+	document.getElementById('body').style.color = "#ffffff";
+	document.getElementById('brand-name').style.color = "#ffffff";
+	// document.getElementById('nav-items').style.color = "#ffffff";
+}
