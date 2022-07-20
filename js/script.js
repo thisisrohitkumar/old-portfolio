f095d2f634bc7be95762817eeffc231d68d3121c
Rohit Kumar <107547744+thisisrohitkumar@users.noreply.github.com>
2022-07-20 12:44:06 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ function darkModeOn(){
 	document.getElementById('body').style.backgroundColor = "#333333";
 	document.getElementById('body').style.color = "#ffffff";
 	document.getElementById('brand-name').style.color = "#ffffff";
-	// document.getElementById('nav-items').style.color = "#ffffff";
+	document.getElementById('nav-items').style.color = "#ffffff";
 }
